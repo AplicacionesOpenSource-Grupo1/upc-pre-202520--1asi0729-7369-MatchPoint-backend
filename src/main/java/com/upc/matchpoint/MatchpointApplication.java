936c6d761e01533357232388b19e1e7d68a8a49e
Juan Angulo <juancarlosanguloabud@gmail.com>
2025-11-10 22:08:22 -0500

    chore: inital project configuration

---
+++ src/main/java/com/upc/matchpoint/MatchpointApplication.java
@@ -0,0 +1,13 @@
+package com.upc.matchpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchpointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchpointApplication.class, args);
+    }
+
+}
